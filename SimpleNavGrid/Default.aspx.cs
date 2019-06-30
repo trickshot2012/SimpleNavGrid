297afb5fc211a581f998d12014231df2154a12d6
trickshot2012 <trickshot2012@yahoo.de>
2019-06-30 11:28:36 +0200

    Projektdateien hinzufügen.

---
+++ SimpleNavGrid/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleNavGrid
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        static string sTable;
+        string conStrg = "Data Source=.\\sqlexpress;Initial Catalog=Test;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlDataSource1.ConnectionString = conStrg;
+            if (IsPostBack && sTable != "" && sTable != null)
+            {
+                // autogenerate 
+                SqlDataSource1.SelectCommand = "select * from " + sTable;
+                SqlDataSource1.DeleteCommand = "delete " + sTable + " where id=@id";
+                SqlDataSource1.UpdateCommand = "update " + sTable + " set para1=@para1 where id=@id";
+            }
+        }
+
+        protected void TreeView1_SelectedNodeChanged(object sender, EventArgs e)
+        {
+            sTable = TreeView1.SelectedValue;
+            loaddata();
+        }
+
+        void loaddata()
+        {
+            SqlDataSource1.SelectCommand = "select * from " + sTable;
+            SqlDataSource1.DataBind();
+            GridView1.DataBind();
+        }
+    }
+}
---
+++ SimpleNavGrid/Default.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Dieser Code wurde von einem Tool generiert.
+//
+//     Änderungen an dieser Datei können fehlerhaftes Verhalten verursachen und gehen verloren, wenn
+//     der Code neu generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace SimpleNavGrid {
+    
+    
+    public partial class Default {
+        
+        /// <summary>
+        /// form1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// TreeView1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TreeView TreeView1;
+        
+        /// <summary>
+        /// GridView1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// SqlDataSource1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+    }
+}
